8650e1186e5c1af5829b7c36ea6d6cafea052d3d
Devin <devin.d.x.li@gmail.com>
2019-08-11 23:12:37 +0800

    Add Project1.1_What's_in_the_basic_template

---
+++ Project1.1_What's_in_the_basic_template/Project1.1_What's_in_the_basic_template/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Project1.1_What's_in_the_basic_template
+//
+//  Created by XDLee on 2019/8/11.
+//  Copyright © 2019 XDLee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
