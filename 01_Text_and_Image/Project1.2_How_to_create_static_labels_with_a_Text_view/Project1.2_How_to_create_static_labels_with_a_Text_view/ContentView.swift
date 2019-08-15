64b436bd1ad61a52e949024114b9dfa6c703b546
Devin <devin.d.x.li@gmail.com>
2019-08-15 23:40:56 +0800

    Add Project1.3_How_to_style_text_views

--- 01_Text_and_Image/Project1.2_How_to_create_static_labels_with_a_Text_view/Project1.2_How_to_create_static_labels_with_a_Text_view/ContentView.swift
+++ 01_Text_and_Image/Project1.2_How_to_create_static_labels_with_a_Text_view/Project1.2_How_to_create_static_labels_with_a_Text_view/ContentView.swift
@@ -2,15 +2,15 @@
 //  ContentView.swift
 //  Project1.2_How_to_create_static_labels_with_a_Text_view
 //
-//  Created by XDLee on 2019/8/12.
-//  Copyright © 2019 XDLee. All rights reserved.
-//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+//        Text("Hello World")
+        Text("This is an extremely long string that will never fit even the widest of Phones.")
+            .lineLimit(0)
+            .truncationMode(.middle)
     }
 }
 
