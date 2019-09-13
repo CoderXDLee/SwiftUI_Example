2b93945db7259b3c27617313d4d1564d61a94731
devin.d.x.li <devin.d.x.li@gmail.com>
2019-09-13 23:35:36 +0800

    Add Project1.1

---
+++ 01_Text_and_Image/Project1.1_What's_in_the_basic_template?/Project1.1_What's_in_the_basic_template?/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project1.1_What's_in_the_basic_template?
+//
+//  Created by Devin X D LI on 2019/9/13.
+//  Copyright © 2019 Devin X D LI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
