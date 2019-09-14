3b7708b5472eab7cc60c4d09754be6add6ebbc11
devin.d.x.li <devin.d.x.li@gmail.com>
2019-09-14 23:48:03 +0800

    Create Project1.2

---
+++ 01_Text_and_Image/Project1.2_How_to_create_static_labels_with_a_Text_view/Project1.2_How_to_create_static_labels_with_a_Text_view/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project1.2_How_to_create_static_labels_with_a_Text_view
+//
+//  Created by Devin X D LI on 2019/9/14.
+//  Copyright © 2019 Devin X D LI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
