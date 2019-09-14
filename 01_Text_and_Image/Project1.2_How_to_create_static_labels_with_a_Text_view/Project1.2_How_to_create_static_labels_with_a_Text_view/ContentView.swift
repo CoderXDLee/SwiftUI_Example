38fa706c6b677e0983f50f8e3bf80fa4ad2c4d8e
devin.d.x.li <devin.d.x.li@gmail.com>
2019-09-15 00:12:40 +0800

    Update 1.2

--- 01_Text_and_Image/Project1.2_How_to_create_static_labels_with_a_Text_view/Project1.2_How_to_create_static_labels_with_a_Text_view/ContentView.swift
+++ 01_Text_and_Image/Project1.2_How_to_create_static_labels_with_a_Text_view/Project1.2_How_to_create_static_labels_with_a_Text_view/ContentView.swift
@@ -2,15 +2,15 @@
 //  ContentView.swift
 //  Project1.2_How_to_create_static_labels_with_a_Text_view
 //
-//  Created by Devin X D LI on 2019/9/14.
-//  Copyright © 2019 Devin X D LI. All rights reserved.
-//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+//        Text("Hello World")
+        Text("This is an extremely long string that will never fit even the widest of Phones.")
+            .lineLimit(0)
+            .truncationMode(.middle)
     }
 }
 
