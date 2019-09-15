65f20cc2b6fc93506442b120c64e47091c773db3
devin.d.x.li <devin.d.x.li@gmail.com>
2019-09-15 16:23:10 +0800

    Add Project1.3

---
+++ 01_Text_and_Image/Project1.3_How_to_style_text_views_with_fonts_colors_linespacing_and_more/Project1.3_How_to_style_text_views_with_fonts_colors_linespacing_and_more/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project1.3_How_to_style_text_views_with_fonts_colors_linespacing_and_more
+//
+//  Created by Devin X D LI on 2019/9/15.
+//  Copyright © 2019 Devin X D LI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
