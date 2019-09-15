96d9a0349458bc6ac6b254ca423f7a2903c99244
devin.d.x.li <devin.d.x.li@gmail.com>
2019-09-15 17:24:09 +0800

    Add content for Project1.5

--- 01_Text_and_Image/Project1.5_How_to_draw_images_using_Image_views/Project1.5_How_to_draw_images_using_Image_views/ContentView.swift
+++ 01_Text_and_Image/Project1.5_How_to_draw_images_using_Image_views/Project1.5_How_to_draw_images_using_Image_views/ContentView.swift
@@ -2,15 +2,30 @@
 //  ContentView.swift
 //  Project1.5_How_to_draw_images_using_Image_views
 //
-//  Created by Devin X D LI on 2019/9/15.
-//  Copyright © 2019 Devin X D LI. All rights reserved.
-//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        // 1.从 Bundle 中加载
+//        Image("example-image")
+        
+        // 2.从 System icons 加载
+//        Image(systemName: "cloud.heavyrain.fill")
+        
+        // 3.从 UIImage 加载
+//        guard let image = UIImage(named: "example-image") else {
+//            fatalError("Unable to load image")
+//        }
+//        return Image(uiImage: image)
+        
+        // 4.对系统图标着色
+//        Image(systemName: "cloud.heavyrain.fill")
+//            .foregroundColor(.red)
+        
+        // 5.通过对系统图标设置动态字体来放大图像
+        Image(systemName: "cloud.heavyrain.fill")
+            .font(.largeTitle)
     }
 }
 
