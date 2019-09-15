1013323dab21affe7f72a4a668c57c344dba63d2
devin.d.x.li <devin.d.x.li@gmail.com>
2019-09-15 17:07:49 +0800

    Add `Project1.5_How_to_draw_images_using_Image_views`

---
+++ 01_Text_and_Image/Project1.5_How_to_draw_images_using_Image_views/Project1.5_How_to_draw_images_using_Image_views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project1.5_How_to_draw_images_using_Image_views
+//
+//  Created by Devin X D LI on 2019/9/15.
+//  Copyright © 2019 Devin X D LI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
