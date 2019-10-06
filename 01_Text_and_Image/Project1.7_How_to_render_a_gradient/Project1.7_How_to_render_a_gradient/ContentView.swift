377fd8167fc7ace91ca1eb9bd1c0c3c14e9b9d39
Devin <devin.d.x.li@gmail.com>
2019-10-06 22:16:00 +0800

    Update Project1.7

--- 01_Text_and_Image/Project1.7_How_to_render_a_gradient/Project1.7_How_to_render_a_gradient/ContentView.swift
+++ 01_Text_and_Image/Project1.7_How_to_render_a_gradient/Project1.7_How_to_render_a_gradient/ContentView.swift
@@ -26,10 +26,29 @@ struct ContentView: View {
 //            .background(LinearGradient(gradient: Gradient(colors: [.white, .red, .black]), startPoint: .leading, endPoint: .trailing))
         
         // 4. 对角线性渐变: white -> red -> black
-        Text("Hello World")
-            .padding()
-            .foregroundColor(.white)
-            .background(LinearGradient(gradient: Gradient(colors: [.white, .red, .black]), startPoint: .topLeading, endPoint: .bottomTrailing))
+//        Text("Hello World")
+//            .padding()
+//            .foregroundColor(.white)
+//            .background(LinearGradient(gradient: Gradient(colors: [.white, .red, .black]), startPoint: .topLeading, endPoint: .bottomTrailing))
+        
+        // 5. 径向渐变
+//        let colors = Gradient(colors: [.red, .yellow, .green, .blue, .purple])
+//        let conic = RadialGradient(gradient: colors, center: .center, startRadius: 50, endRadius: 200)
+//        return Circle()
+//            .fill(conic)
+//            .frame(width: 400, height: 400)
+        
+        // 6. 圆锥梯度
+//        let colors = Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red])
+//        let conic = AngularGradient(gradient: colors, center: .center)
+//        return Circle()
+//            .fill(conic)
+        
+        // 7. 圆环
+        let colors = Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red])
+        let conic = AngularGradient(gradient: colors, center: .center, startAngle: .zero, endAngle: .degrees(360))
+        return Circle()
+            .strokeBorder(conic, lineWidth: 50)
     }
 }
 
