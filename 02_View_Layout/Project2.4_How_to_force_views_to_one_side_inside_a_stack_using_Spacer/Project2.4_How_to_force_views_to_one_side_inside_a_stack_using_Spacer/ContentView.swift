3468862c21ced6f51eb6f64343277315b06494c6
Devin <devin.d.x.li@gmail.com>
2019-10-12 21:32:33 +0800

    Create Project2.4_How_to_force_views_to_one_side_inside_a_stack_using_Spacer

---
+++ 02_View_Layout/Project2.4_How_to_force_views_to_one_side_inside_a_stack_using_Spacer/Project2.4_How_to_force_views_to_one_side_inside_a_stack_using_Spacer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project2.4_How_to_force_views_to_one_side_inside_a_stack_using_Spacer
+//
+//  Created by Devin X D LI on 2019/10/12.
+//  Copyright © 2019 Devin X D LI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
