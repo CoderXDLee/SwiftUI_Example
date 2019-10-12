acf3e8fc8615c40d206861bb1aad4bee94ec53da
Devin <devin.d.x.li@gmail.com>
2019-10-12 23:15:52 +0800

    Add content for 2.4

--- 02_View_Layout/Project2.4_How_to_force_views_to_one_side_inside_a_stack_using_Spacer/Project2.4_How_to_force_views_to_one_side_inside_a_stack_using_Spacer/ContentView.swift
+++ 02_View_Layout/Project2.4_How_to_force_views_to_one_side_inside_a_stack_using_Spacer/Project2.4_How_to_force_views_to_one_side_inside_a_stack_using_Spacer/ContentView.swift
@@ -2,15 +2,37 @@
 //  ContentView.swift
 //  Project2.4_How_to_force_views_to_one_side_inside_a_stack_using_Spacer
 //
-//  Created by Devin X D LI on 2019/10/12.
-//  Copyright © 2019 Devin X D LI. All rights reserved.
-//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        
+//        // 1. 不使用 Spacer
+//        VStack {
+//            Text("Hello World")
+//        }
+        
+//        // 2. 使用 Spacer 将文本视图推到父视图顶部
+//        VStack {
+//            Text("Hello World")
+//            Spacer()
+//        }
+        
+        // 3. 在 HStack 中使用 Spacer
+//        HStack {
+//            Text("Hello")
+//            Spacer()
+//            Text("World")
+//        }
+        
+        // 4. 多个 Spacer 分割空间
+        VStack {
+            Spacer()
+            Text("Hello world.")
+            Spacer()
+            Spacer()
+        }
     }
 }
 
