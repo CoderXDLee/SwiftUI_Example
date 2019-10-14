6a5cc7a8b941c88d48d14c9c40d09b010f2de4f8
Devin <devin.d.x.li@gmail.com>
2019-10-15 00:00:07 +0800

    Add Project2.12_How_to_provide_relative_sizes_using_GeometryReader

---
+++ 02_View_Layout/Project2.12_How_to_provide_relative_sizes_using_GeometryReader/Project2.12_How_to_provide_relative_sizes_using_GeometryReader/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Project2.12_How_to_provide_relative_sizes_using_GeometryReader
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            HStack(spacing: 0) {
+                Text("Left")
+                    .frame(width: geometry.size.width / 2, height: 50)
+                    .background(Color.orange)
+                Text("Right")
+                    .frame(width: geometry.size.width / 2, height: 50)
+                    .background(Color.yellow)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
