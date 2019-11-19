07fe68016c59090a437396ee8333d15c36707334
Devin <devin.d.x.li@gmail.com>
2019-11-19 09:26:57 +0800

    Add Project12_How_to_read_tap_and_double-tap_gestures

---
+++ 03_Responding_to_events/Project12_How_to_read_tap_and_double-tap_gestures/Project12_How_to_read_tap_and_double-tap_gestures/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Project12_How_to_read_tap_and_double-tap_gestures
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        // 1. 给文本视图添加 tap 手势
+//        Text("Tap me!")
+//            .onTapGesture {
+//                print("Tapped!")
+//            }
+        
+        Image("example-image")
+            .onTapGesture(count: 2) {
+                print("Double tapped!")
+            }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
